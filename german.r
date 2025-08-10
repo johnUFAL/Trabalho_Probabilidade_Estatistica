15f92108e837c72378ad1544b90f0578fcee947e
johnUFAL <jvdn@ic.ufal.br>
2025-08-10 19:56:36 -0300

    Questão 3

--- german.r
+++ german.r
@@ -18,15 +18,16 @@ summary(df) # Est. Descritiva
 # Tratamento
 colSums(is.na(df)) # Verifica dados faltantes
 
-  # Verificando outliers 
+par(mar = c(4, 4, 2, 1))
+
+# Verificando outliers 
 boxplot(df$Meses_existencia, main =  "Outliers em Meses Existencia")
 boxplot(df$Valor_credito, main =  "Outliers em Valor_credito")
 boxplot(df$Taxa_parcela_renda, main =  "Outliers em Taxa_parcela_renda")
 boxplot(df$Residencia_atual, main =  "Outliers em Residencia_atual")
 boxplot(df$Idade, main =  "Outliers em Idade")
 boxplot(df$Credito_existente, main =  "Outliers em Credito_existente")
-boxplot(df$Dependes, main =  "Outliers em Dependes")
-
+boxplot(df$Dependentes, main =  "Outliers em Dependentes") 
 
 # Usando valor crédito: valor min, valor max, média, mediana. desvio padrão, os quartis, histograma, boxplot e interpretação
 min(df$Valor_credito)
@@ -35,7 +36,6 @@ mean(df$Valor_credito)
 median(df$Valor_credito)
 sd(df$Valor_credito)
 quantile(df$Valor_credito, probs = c(0.25, 0.50, 0.75))
-par(mar = c(4, 4, 2, 1))
 hist(df$Valor_credito,
      breaks = 30,
      main = "Dsitribuição de Frequência",
@@ -46,3 +46,26 @@ boxplot(df$Valor_credito,
         main = "Distribuição Valor Crédito",
         ylab = "Valores (DM)", 
         col = "lightgreen")
+
+# Analise bivariada:  "Saldo_poupanca" e "Tempo_emprego"
+str(df[c("Saldo_poupanca", "Tempo_emprego")])
+
+tbCont <- table(df$Saldo_poupanca, df$Tempo_emprego)
+print(tbCont)
+addmargins((tbCont)) #soma total
+
+prop.table(tbCont)
+prop.table(tbCont, margin = 1)
+prop.table(tbCont, margin = 2)
+
+tst <- chisq.test(tbCont) # Associação significativa
+print(tst) # Associação sig == p < 0.05
+tst$expected # Esperados
+
+ggplot(as.data.frame(tbCont), 
+       aes(x = Var1, y = Freq, fill = Var2)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Saldo Poupança", y = "Frequência", 
+       fill = "Tempo Emprego", 
+       title = "Relação Saldo Poupança x Tempo Emprego") +
+  theme_minimal()
