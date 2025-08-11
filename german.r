22c5eaf607cb650e8e69048be14b4cf44114d9e6
johnUFAL <jvdn@ic.ufal.br>
2025-08-11 14:49:39 -0300

    Qeustao 5

--- german.r
+++ german.r
@@ -1,13 +1,29 @@
+## Trabalho de P&E - German Credit Data
+
+rm(list = ls()) # Limpeza
+dev.off() # Erros gráficos
+
+
 # imporntando, lende e preparando o dataframe
 setwd("C:/Users/Joao Duarte/Desktop/Trabalho R/Trabalho_Probabilidade_Estatistica")
 
 df <- read.table("german.data", header = FALSE, sep = " ")
 
-colnames(df) <- c(  "Status_conta", "Meses_existencia", "Historico_credito", "Proposito",
-                    "Valor_credito", "Saldo_poupanca", "Tempo_emprego", "Taxa_parcela_renda",
-                    "Status_pessoal_sexo", "Fiador", "Residencia_atual", "Bens",
-                    "Idade", "Planos de parcelamento", "Moradia", "Credito_existente", "Emprego", "Dependentes",
-                    "Telefone", "Trabalhador_estrangeiro", "Classe")
+colnames(df) <- c(
+  "Status_conta", "Meses_existencia", "Historico_credito", "Proposito",
+  "Valor_credito", "Saldo_poupanca", "Tempo_emprego", "Taxa_parcela_renda",
+  "Status_pessoal_sexo", "Fiador", "Residencia_atual", "Bens",
+  "Idade", "Planos_parcelamento", "Moradia", "Credito_existente", 
+  "Emprego", "Dependentes", "Telefone", "Trabalhador_estrangeiro", "Classe"
+)
+
+# Carregando pacotes
+library(ggplot2)
+library(corrplot)
+
+# Comfiguração gráfica
+par(mar = c(4, 4, 2, 1))
+theme_set(theme_minimal()) #padrão ggplot
 
 # Analise bruta
 head(df) # Primeiras linhas
@@ -16,18 +32,17 @@ str(df) # Estrutura do df
 summary(df) # Est. Descritiva
 
 # Tratamento
-colSums(is.na(df)) # Verifica dados faltantes
+print(colSums(is.na(df))) # Verifica dados faltantes
+print(sapply(df, class)) 
 
-par(mar = c(4, 4, 2, 1))
+par(mfrow = c(2, 4))
+var_numericas <- c("Meses_existencia", "Valor_credito", "Taxa_parcela_renda",
+                   "Residencia_atual", "Idade", "Credito_existente", "Dependentes")
 
-# Verificando outliers 
-boxplot(df$Meses_existencia, main =  "Outliers em Meses Existencia")
-boxplot(df$Valor_credito, main =  "Outliers em Valor_credito")
-boxplot(df$Taxa_parcela_renda, main =  "Outliers em Taxa_parcela_renda")
-boxplot(df$Residencia_atual, main =  "Outliers em Residencia_atual")
-boxplot(df$Idade, main =  "Outliers em Idade")
-boxplot(df$Credito_existente, main =  "Outliers em Credito_existente")
-boxplot(df$Dependentes, main =  "Outliers em Dependentes") 
+for (var in var_numericas) {
+  boxplot(df[[var]], main = paste("Outliers em", var), ylab = var)
+}
+par(mfrow = c(1, 1))
 
 # Usando valor crédito: valor min, valor max, média, mediana. desvio padrão, os quartis, histograma, boxplot e interpretação
 min(df$Valor_credito)
@@ -36,13 +51,15 @@ mean(df$Valor_credito)
 median(df$Valor_credito)
 sd(df$Valor_credito)
 quantile(df$Valor_credito, probs = c(0.25, 0.50, 0.75))
+
 hist(df$Valor_credito,
      breaks = 30,
      main = "Dsitribuição de Frequência",
      xlab = "Valor Crédito (DM)", 
      col = "lightblue")
+
 boxplot(df$Valor_credito, 
-        ylin = c(0, 20000),
+        ylim = c(0, 20000),
         main = "Distribuição Valor Crédito",
         ylab = "Valores (DM)", 
         col = "lightgreen")
@@ -52,15 +69,12 @@ str(df[c("Saldo_poupanca", "Tempo_emprego")])
 
 tbCont <- table(df$Saldo_poupanca, df$Tempo_emprego)
 print(tbCont)
-addmargins((tbCont)) #soma total
+print(addmargins(tbCont)) #soma total
 
-prop.table(tbCont)
-prop.table(tbCont, margin = 1)
 prop.table(tbCont, margin = 2)
 
 tst <- chisq.test(tbCont) # Associação significativa
 print(tst) # Associação sig == p < 0.05
-tst$expected # Esperados
 
 ggplot(as.data.frame(tbCont), 
        aes(x = Var1, y = Freq, fill = Var2)) +
@@ -70,12 +84,14 @@ ggplot(as.data.frame(tbCont),
        title = "Relação Saldo Poupança x Tempo Emprego") +
   theme_minimal()
 
-library(ggplot2)
 
 # Analise em boxplot das variáveis Idade e saldo_poupanca
-df$Saldo_poupanca <- factor(df$Saldo_poupanca, 
-                            levels = c("A61", "A62", "A63", "A64", "A65"),
-                            labels = c("<100 DM", "100-500 DM", "500-1000 DM", "≥1000 DM", "Desconhecido"))
+df$Saldo_poupanca <- factor(
+  df$Saldo_poupanca,
+  levels = c("A61", "A62", "A63", "A64", "A65"),
+  labels = c("<100 DM", "100-500 DM", "500-1000 DM", "≥1000 DM", "Desconhecido"),
+  exclude = NULL
+)
 
 ggplot(df, aes(x = Saldo_poupanca, y = Idade, fill = Saldo_poupanca)) +
   geom_boxplot() + labs(title = "Idade por Saldo em poupança",
@@ -86,5 +102,22 @@ ggplot(df, aes(x = Saldo_poupanca, y = Idade, fill = Saldo_poupanca)) +
   theme_minimal() +
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust = 1))
-            
-                            
+
+# Matriz de correlação
+var_cor <- df[, c("Meses_existencia", "Valor_credito", "Taxa_parcela_renda",
+                        "Residencia_atual", "Idade", "Credito_existente", "Dependentes")]
+
+mat <- cor(var_cor)
+
+corrplot(mat, 
+         method = "color", 
+         type = "upper", 
+         tl.col = "black",
+         tl.srt = 45,
+         addCoef.col = "black",
+         number.cex = 0.5,
+         diag = FALSE,
+         col = colorRampPalette(c("blue", "white", "red"))(200),
+         mar = c(0, 0 , 2, 0),
+         title = "Matriz de Correlação",
+         cl.ratio  = 0.2)
